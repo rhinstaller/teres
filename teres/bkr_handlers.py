2bbde619ba799de8024c2ac14bc76d1016f726ee
Peter Kotvan <pkotvan@redhat.com>
2016-04-20 18:42:47 +0200

    Add asynchronous communication with beaker.
    Create a thread that reads continuously records from a queue and
    communicate with beaker. In this way the library doesn't block the main
    program.

--- teres/bkr_handlers.py
+++ teres/bkr_handlers.py
@@ -27,6 +27,9 @@ import requests
 import libxml2
 import logging
 import teres
+import Queue
+import threading
+import time
 
 # Flags defintion
 TASK_LOG_FILE = object()  # boolean
@@ -34,6 +37,11 @@ SUBTASK_RESULT = object()  # optional parameter: path
 SCORE = object()  # mandatory parameter: score
 SUBTASK_LOG_FILE = object()  # optional parameter: result url
 DEFAULT_LOG_DEST = object()  # boolean
+
+# Define record types since we need to propagate information about the type from
+# the parent class.
+_LOG = object()
+_FILE = object()
 
 logger = logging.getLogger(__name__)
 
@@ -88,8 +96,13 @@ class ThinBkrHandler(teres.Handler):
                  task_log_dir="/tmp/",
                  recipe_id=None,
                  lab_controller_url=None,
-                 disable_subtasks=False):
+                 disable_subtasks=False,
+                 flush_delay=15):
         super(ThinBkrHandler, self).__init__(result_level, process_logs)
+
+        # This is a thread safe queue to pass logs and files to thread that
+        # takes care of sending them to beaker.
+        self.record_queue = Queue.Queue()
 
         # Read beaker environment variables to be able to communicate with lab
         # controller.
@@ -108,6 +121,13 @@ class ThinBkrHandler(teres.Handler):
         task_log_prefix = task_log_name + "."
         self.task_log = tempfile.TemporaryFile(prefix=task_log_prefix,
                                                dir=self.task_log_dir)
+
+        # Keep track whether the thread is already finished. Prepare and run the
+        # thread loop.
+        self.finished = False
+        self.flush_delay = flush_delay
+        self.async_thread = threading.Thread(target=self._thread_loop)
+        self.async_thread.start()
 
     def _get_recipe(self):
         """
@@ -168,7 +188,15 @@ class ThinBkrHandler(teres.Handler):
             return self._get_task_url() + "results/"
 
     def _emit_log(self, record):
-        """Record a result to beaker and return an url with a result id."""
+        """
+        Pass log records to the record_queue.
+        """
+        self.record_queue.put((_LOG, record))
+
+    def _thread_emit_log(self, record):
+        """
+        Send log record to beaker.
+        """
 
         # Without any flags specified just write the message into the log file.
         self.task_log.write(_format_msg(record))
@@ -197,11 +225,17 @@ class ThinBkrHandler(teres.Handler):
             self.last_result_url = req.headers["Location"] + "/"
 
             if record.flags.get(DEFAULT_LOG_DEST, False):
-                self.default_log_dest = req.headers["Location"] + "/"
+                self.default_log_dest = self.last_result_url
 
     def _emit_file(self, record):
         """
-        This method sends file to beaker.
+        Pass file records to the record_queue.
+        """
+        self.record_queue.put((_FILE, record))
+
+    def _thread_emit_file(self, record):
+        """
+        Send file record to beaker.
         """
         if record.logname is None:
             if isinstance(record.logfile, str):
@@ -238,11 +272,54 @@ class ThinBkrHandler(teres.Handler):
         """
         self.default_log_dest = self._get_task_url()
 
-    def close(self):
+    def flush(self):
+        """
+        Send current state of the task log file to beaker whenever this is
+        called. This is meant to enable continuous updating of the task log.
+        """
         record = teres.ReportRecord(teres.FILE,
                                     None,
                                     logfile=self.task_log,
                                     logname=self.task_log_name)
+
         self._emit_file(record)
 
+    def close(self):
+        """
+        Set handler state to finished. Join the thread for asynchronous
+        communication with beaker and finally close task log.
+        """
+        self.finished = True
+        self.async_thread.join()
         self.task_log.close()
+
+    def _thread_loop(self):
+        """
+        This is a thread loop for sending records to beaker asynchronously. In
+        this way the program using this handler isn't blocked by the
+        communication. Although it has to wait for the join when self.close() is
+        called.
+        """
+        flushed = True
+        last_update = time.clock()
+
+        while not (self.finished and self.record_queue.empty()):
+            try:
+                record_type, record = self.record_queue.get(timeout=1)
+
+                if record_type == _LOG:
+                    self._thread_emit_log(record)
+                    flushed = False
+                elif record_type == _FILE:
+                    self._thread_emit_file(record)
+
+            except Queue.Empty:
+                pass
+
+            if flushed and (time.clock() - last_update > self.flush_delay):
+                self.flush()
+                flushed = True
+                last_update = time.clock()
+
+        if not flushed:
+            self.flush()
